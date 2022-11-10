caaf2775aca894ff483928f16c1c3f2cbad7020b
Lara Ledezma <lale7867@colorado.edu>
2022-11-10 09:49:02 -0700

    Update

---
+++ assignment12/assignment12.js
@@ -0,0 +1,23 @@
+function check() {
+    console.log('test');
+}
+
+function yes() {
+    alert(output.textContent);
+}
+
+function Random() {
+    return Math.floor(Math.random() * 9000000000) + 1000000000;
+  }
+  
+  function randomValue() {
+    document.getElementById('tb').value = Random();
+  }
+
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const yesButton = document.querySelector('.yes-button').addEventListener('click', yes);
+const noButton = document.querySelector('.no-button').addEventListener('click', no);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
