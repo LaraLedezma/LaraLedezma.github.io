6a7688bda5e0be887619edae20c0b8d22f44a6a8
Lara Ledezma <lale7867@colorado.edu>
2022-11-13 13:45:41 -0700

    Update

--- assignment12/assignment12.js
+++ assignment12/assignment12.js
@@ -8,16 +8,15 @@ function yes() {
 
 function Random() {
     return Math.floor(Math.random() * 9000000000) + 1000000000;
-  }
+}
   
-  function randomValue() {
+function randomValue() {
     document.getElementById('tb').value = Random();
-  }
+}
 
 const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
 const yesButton = document.querySelector('.yes-button').addEventListener('click', yes);
-const noButton = document.querySelector('.no-button').addEventListener('click', no);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+const noButton = document.querySelector('.no-button').addEventListener('click', randomValue);
